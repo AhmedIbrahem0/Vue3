c1fb578b7fb7f713823d6940291a1c27165ca772
Ahmed Ibrahem <51453028+AhmedIbrahem0@users.noreply.github.com>
2022-11-20 21:15:53 +0200

    Update web.php
    main updated

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+ Route::view('/mainBranch','index')->name('index')->where('any','.*');
  Route::view('/{any?}','index')->name('index')->where('any','.*');
 
